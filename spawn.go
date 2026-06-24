c4faf71cd03d12deba4474f8a02feb14202c9450
Omar Polo <op@omarpolo.com>
2026-06-24 19:04:45 +0000

    spawn: use 127.0.0.1:0 instead of stdio as grpc address

--- spawn.go
+++ spawn.go
@@ -65,7 +65,7 @@ func spawn(ctx context.Context, exe string, args []string) (*grpc.ClientConn, er
 
 	conn := newStdioConn(stdin, stdout, cmd, nil)
 
-	client, err := grpc.NewClient("stdio",
+	client, err := grpc.NewClient("127.0.0.1:0",
 		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
 			return conn, nil
 		}),
